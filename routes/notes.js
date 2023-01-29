c51ccf19bb036c76871478da8d2bf53baebccbd8
cwen13 <cwenrich13@gmail.com>
2023-01-29 06:21:43 -0500

    Seting base files and structure

---
+++ routes/notes.js
@@ -0,0 +1,41 @@
+// notes api
+const notes = require('express').Router();
+//const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+//const uuid = require('../helpers/uuid');
+
+// /api/feedback
+// GET Route for retrieving all the feedback
+notes.get('/', (req, res) => {
+  console.info(`${req.method} request received for feedback`);
+
+  readFromFile('./db/feedback.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for submitting feedback
+notes.post('/', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to submit feedback`);
+
+  // Destructuring assignment for the items in req.body
+  const { } = req.body;
+
+  // If all the required properties are present
+  if () {
+    // Variable for the object we will save
+    const newNote = {
+    };
+
+    readAndAppend(newFeedback, './db/feedback.json');
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting feedback');
+  }
+});
+
+module.exports = notes;
