d99179d49867b1f5ffbb47b41efa025cf3da3a78
cwen13 <cwenrich13@gmail.com>
2023-01-29 20:08:51 -0500

    Working on JS

--- routes/notes.js
+++ routes/notes.js
@@ -12,56 +12,27 @@ notes.get("/", (req,res) => {
 
 notes.post("/", (req,res) => {
   console.log(req.body);
+  const {title, text} = req.body;
 
-  const {noteTitle, noteText} = req.body;
-  if (noteTitle && noteText) {
+  if (title && text) {
     const newNote = {
-      "title": noteTitle,
-      "text": noteText
+      title,
+      text
     };
-    readAndAppend(newNote, "./db/db.json");
 
+    readAndAppend(newNote, "./db/db.json");
+    
     const response = {
       status: "sucess",
       body: newNote
-
-// notes api
-const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
-
-// /api/feedback
-// GET Route for retrieving all the feedback
-notes.get('/', (req, res) => {
-  console.info(`${req.method} request received for feedback`);
-
-  readFromFile('./db/feedback.json').then((data) => res.json(JSON.parse(data)));
-});
-
-// POST Route for submitting feedback
-notes.post('/', (req, res) => {
-  // Log that a POST request was received
-  console.info(`${req.method} request received to submit feedback`);
-
-  // Destructuring assignment for the items in req.body
-  const { } = req.body;
-
-  // If all the required properties are present
-  if () {
-    // Variable for the object we will save
-    const newNote = {
     };
-
-    readAndAppend(newFeedback, './db/feedback.json');
-
-    const response = {
-      status: 'success',
-      body: newNote,
-    };
-    
     res.json(response);
   } else {
     res.json("Error in posting new note");
-  }   
+  }
 });
 
-module.exports = notes;
+//notes.delete("/:id", (req,re) => { 
+//
+//}
+  
